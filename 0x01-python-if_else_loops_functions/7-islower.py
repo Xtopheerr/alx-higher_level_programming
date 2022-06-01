67b904d36db3bf924512af9e1034736f33c949c0
Muoneke Christopher <98641970+Xtopheerr@users.noreply.github.com>
2022-05-31 21:19:48 -0700

    Update 7-islower.py

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,1 +1,9 @@
-i
+#!/usr/bin/python3
+# Author - Muoneke Christopher
+
+def islower(c):
+    """Function checks for lowercase characters."""
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
