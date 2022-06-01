bbbe85afcd04b40e919750b80ef21e9e607f6c4f
Muoneke Christopher <98641970+Xtopheerr@users.noreply.github.com>
2022-05-31 21:08:40 -0700

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,1 +1,5 @@
-i
+#!/usr/bin/python3
+# Author - Muoneke Christopher
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")
