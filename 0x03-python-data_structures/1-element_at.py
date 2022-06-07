a78a703aec2d9508e68e64d572dd875d0bbfcadb
Muoneke Christopher <98641970+Xtopheerr@users.noreply.github.com>
2022-06-06 19:50:41 -0700

    Update 1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,1 +1,8 @@
-i
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+    """etrieves an element from a list like in C."""
+    if idx < 0 or idx > (len(my_list) - 1):
+        return None
+    else:
+        return (my_list[idx])
