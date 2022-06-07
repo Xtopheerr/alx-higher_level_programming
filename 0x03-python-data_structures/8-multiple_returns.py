f7cc4e2daf8b88a824851b98fa20a294702cd67a
Muoneke Christopher <98641970+Xtopheerr@users.noreply.github.com>
2022-06-06 20:05:09 -0700

    Update 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,1 +1,16 @@
-i
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    tup = ()
+    length = len(sentence)
+    first = ""
+
+    if not sentence:
+        length = 0
+        first = None
+        tup = tup + (length, first)
+
+    else:
+        first = sentence[0]
+        tup = tup + (length, first)
+
+    return tup
